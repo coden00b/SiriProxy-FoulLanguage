349ec41326209d9077f53df3978482a31684155a
Jimmy Fox <jwfox5150@me.com>
2011-12-13 15:47:16 -0500

    development

--- lib/siriproxy-foullanguage.rb
+++ lib/siriproxy-foullanguage.rb
@@ -27,7 +27,7 @@ class SiriProxy::Plugin::FoulLanguage < SiriProxy::Plugin
     
   end
 
-  listen_for /fuck/i or /shit/i or /cunt/i or /bitch/i or /bastard/i or /cock/i or /tits/i or /piss/i or /fucker/i do
+  listen_for /fuck/i /shit/i /cunt/i /bitch/i /bastard/i /cock/i /tits/i /piss/i /fucker/i do
     lines = IO.readlines(self.phrase_file)
     rl = rand(lines.count-1)
     say lines[rl]
